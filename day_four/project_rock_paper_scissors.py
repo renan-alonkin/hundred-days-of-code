3066f9a2b30b5ff2fe0a7f58cca722b3451a3f90
Renan Nunes Steinck <renan.alonkin@gmail.com>
2022-05-25 22:02:29 -0300

    Adding a verification if the number is valid on the project rock paper scissors

--- day_four/project_rock_paper_scissors.py
+++ day_four/project_rock_paper_scissors.py
@@ -32,26 +32,30 @@ choices = [rock, paper, scissors]
 
 #Write your code below this line 👇
 users_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
-opponents_choice = random.randint(0, 2)
 
-win_condition = "You lose"
+if users_choice > 2:
+    print("That is an invalid number!")
+else:
+    opponents_choice = random.randint(0, 2)
 
-if(users_choice == 0 and opponents_choice == 2):
-    win_condition = "Rock wins against Sissors, you win!"
+    win_condition = "You lose"
 
-if(users_choice == 1 and opponents_choice == 0):
-    win_condition = "Paper wins against Rock, you win!"
+    if(users_choice == 0 and opponents_choice == 2):
+        win_condition = "Rock wins against Sissors, you win!"
 
-if(users_choice == 2 and opponents_choice == 1):
-    win_condition = "Scissors wins against Paper, you win!"
+    if(users_choice == 1 and opponents_choice == 0):
+        win_condition = "Paper wins against Rock, you win!"
 
-if(users_choice == opponents_choice):
-    win_condition = "It is a tie!"
+    if(users_choice == 2 and opponents_choice == 1):
+        win_condition = "Scissors wins against Paper, you win!"
 
-print("You chose:\n")
-print(choices[users_choice])
+    if(users_choice == opponents_choice):
+        win_condition = "It is a tie!"
 
-print("\nComputer chose:\n")
-print(choices[opponents_choice])
+    print("You chose:\n")
+    print(choices[users_choice])
 
-print(f"\n{win_condition}\n--")
+    print("\nComputer chose:\n")
+    print(choices[opponents_choice])
+
+    print(f"\n{win_condition}\n--")
